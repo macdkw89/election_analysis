2c23a181f4cff7805c8da2c41af28425f852bf4d
macdkw89 <macdkw@gmail.com>
2022-06-22 17:32:53 -0500

    day done?

--- PyPoll.py
+++ PyPoll.py
@@ -1,10 +1,14 @@
-#Below changes my working directory to the appropriate repository
+# Election Analysis Module for UT Data Bootcamp - Kevin MacDonald
+
 import os
 import csv
 import datetime as dt
-now = dt.datetime.now()
-print(now)
-os.chdir('D:/BC/repos/election_analysis')
+
+#now = dt.datetime.now()
+#print(now)
+
+#Below changes my working directory to the appropriate repository --- !!!! Fixed - but keeping code here just in case
+#os.chdir('D:/BC/repos/election_analysis')
 
 # The data we need to retrieve
 # 1. The total number of votes cast
@@ -13,28 +17,24 @@ os.chdir('D:/BC/repos/election_analysis')
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
 
-# Assign a variable for the file to load, and the path
+# Assign a variable for the file to load from a path
 file_to_load = os.path.join('resources','election_results.csv')
+# Assign a variable to save the file to a path
+file_to_save = os.path.join('analysis','election_analysis.txt')
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
+    
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
 
-    # To do: perform analysis
-    print(election_data)
+    # Print each row in the CSV file
+    #for row in file_reader:
+        #print(row)
+    
+    #print the header row
+    headers = next(file_reader)
+    print(headers)
 
-# Create a filename variable to a direct or indirect path to the file
-file_to_save = os.path.join('analysis','election_analysis.txt')
-
-# Using the with statement, open the file as a text file
-with open(file_to_save, "w") as txt_file:
-
-     # Write three counties to the file.
-     txt_file.write("Counties in the Election\n-------------------------------\nArapahoe\nDenver\nJefferson")
-
-
-
-
-
-
-
+    
 
