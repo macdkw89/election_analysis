e4da1f8004bfd6af5c8e85ebfb937569e37dbf67
macdkw89 <macdkw@gmail.com>
2022-06-22 16:40:19 -0500

    testing commit again

--- PyPoll.py
+++ PyPoll.py
@@ -1,3 +1,6 @@
+import os
+os.chdir('D:/BC/repos/election_analysis')
+
 # The data we need to retrieve
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes
@@ -5,10 +8,13 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
 
+#THIS IS THE TEST
+
 #import datetime as dt
 #now = dt.datetime.now()
 #print(now)
-import csv
-file_to_load = "d:/bc/repos/election_analysis/resources/election_results.csv"
+#import csv
+
+file_to_load = "resources/election_results.csv"
 file_variable = open(file_to_load,"r")
 
