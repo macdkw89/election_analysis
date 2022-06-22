97415b10535e0a5b380ab64803fa1cba70780639
macdkw89 <macdkw@gmail.com>
2022-06-22 17:04:37 -0500

    analysis folder created

--- PyPoll.py
+++ PyPoll.py
@@ -22,6 +22,13 @@ with open(file_to_load) as election_data:
     # To do: perform analysis
     print(election_data)
 
+# Create a filename variable to a direct or indirect path to the file
+file_to_save = os.path.join('analysis','election_analysis.txt')
+
+# Using the open() function with the "w" mode we will write data to the file
+open(file_to_save, "w")
+
+
 
 
 
